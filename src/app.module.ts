162d153d21f87c8500db144b2beeaf688268ba15
sangram-jr <sangrambera58@gmail.com>
2026-04-14 01:11:24 +0530

    shift collection & upload module inside modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadModule } from './upload/upload.module';
+//import { UploadModule } from './modules/upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './auth/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CollectionModule } from './collection/collection.module';
+//import { CollectionModule } from './modules/collection/collection.module';
+import { UploadModule } from './modules/upload/upload.module';
+import { CollectionModule } from './modules/collection/collection.module';
 
 
 
